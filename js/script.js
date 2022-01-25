8fb1239adeb3440fbc8300931a8676ef0e6f6327
RayhanKabir29 <rayhan.kabir29@gmail.com>
2022-01-25 23:40:36 +0600

    navbar-fixed

--- js/script.js
+++ js/script.js
@@ -1,42 +1,29 @@
-// search-box open close js code
-let navbar = document.querySelector(".navbar");
-let searchBox = document.querySelector(".search-box .bx-search");
-// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+document.addEventListener("DOMContentLoaded", function(){
+	if (window.innerWidth < 992) {
+	  document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+		everydropdown.addEventListener('hidden.bs.dropdown', function () {
+			this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+			  everysubmenu.style.display = 'none';
+			});
+		})
+	  });
+	  document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+		element.addEventListener('click', function (e) {
+			let nextEl = this.nextElementSibling;
+			if(nextEl && nextEl.classList.contains('submenu')) {	
+			  e.preventDefault();
+			  if(nextEl.style.display == 'block'){
+				nextEl.style.display = 'none';
+			  } else {
+				nextEl.style.display = 'block';
+			  }
+	
+			}
+		});
+	  })
+	}
 
-searchBox.addEventListener("click", ()=>{
-  navbar.classList.toggle("showInput");
-  if(navbar.classList.contains("showInput")){
-    searchBox.classList.replace("bx-search" ,"bx-x");
-  }else {
-    searchBox.classList.replace("bx-x" ,"bx-search");
-  }
-});
-
-// sidebar open close js code
-let navLinks = document.querySelector(".nav-links");
-let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-menuOpenBtn.onclick = function() {
-navLinks.style.left = "0";
-}
-menuCloseBtn.onclick = function() {
-navLinks.style.left = "-100%";
-}
-
-
-// sidebar submenu open close js code
-let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-htmlcssArrow.onclick = function() {
- navLinks.classList.toggle("show1");
-}
-let moreArrow = document.querySelector(".more-arrow");
-moreArrow.onclick = function() {
- navLinks.classList.toggle("show2");
-}
-let jsArrow = document.querySelector(".js-arrow");
-jsArrow.onclick = function() {
- navLinks.classList.toggle("show3");
-}
+	}); 
 // SLider
 $(document).ready(function(){
     
@@ -165,4 +152,4 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
-//Partner
+
